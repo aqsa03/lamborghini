5bd287cc44e10b84b25bc86f623e73df6cda9f8f
aqsa03 <aqsa.parveen@maqsoodlabs.com>
2023-11-08 17:07:24 +0500

    code improvements

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -11,15 +11,16 @@
 <div class="default_table">
     <ol class="table_list">
     <li class="table_list__row">
+    <div class="table_list__row__header_cell">
+            {{ trans('general.parent') }}
+        </div>
         <div class="table_list__row__header_cell">
             {{ trans('general.title') }}
         </div>
         <div class="table_list__row__header_cell">
             {{ trans('general.description') }}
         </div>
-        <div class="table_list__row__header_cell">
-            {{ trans('general.parent') }}
-        </div>
+       
         <div class="table_list__row__header_cell_action">
             {{ trans('general.edit') }}
         </div>
@@ -30,15 +31,16 @@
     </li>
 @foreach ($categories as $category)
     <li class="table_list__row hover:bg-slate-100">
+    <div class="table_list__row__cell">
+            {{  $category->parentCategory->title ?? ''  }}
+        </div>
         <div class="table_list__row__cell">
             {{ $category->title }}
         </div>
         <div class="table_list__row__cell">
             {{ $category->description }}
         </div>
-        <div class="table_list__row__cell">
-            {{ $category->parent_id }}
-        </div>
+       
         <div class="table_list__row__cell">
             <a class="text-amber-700" href="{{ route('categories.edit', $category->id) }}" title="Edit {{ $category->title }}">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
